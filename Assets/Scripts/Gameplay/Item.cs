32fb84bd831d6368af0852ee5b56fb76780b544f
Cade Gordon <cadegordon093@gmail.com>
2022-06-23 22:29:47 -0500

    started creating weapon functionality

---
+++ Assets/Scripts/Gameplay/Item.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    [Header("Item info")]
+    public Sprite _itemIcon;
+    public string _itemName;
+}
---
+++ Assets/Scripts/Gameplay/WeaponItemBehavior.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/Weapon Item")]
+public class WeaponItemBehavior : Item
+{
+    public GameObject modelPrefab;
+    public bool isUnarmed;
+
+}
